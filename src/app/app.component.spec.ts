17ac963ce046239202bc4d9d68ea0bb7b1778bf5
Bernardo <bernaparedesdsns@gmail.com>
2020-11-22 12:27:40 -0600

    Primer commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'App04'`, () => {
+  it(`should have as title 'app04'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('App04');
+    expect(app.title).toEqual('app04');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('App04 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('app04 app is running!');
   });
 });
